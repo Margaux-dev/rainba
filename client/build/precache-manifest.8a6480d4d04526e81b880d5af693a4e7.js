6e3379159ba3d002b8e26d1364d7c52a2f7a4ddf
Margaux Darriberouge <65903630+Margaux-dev@users.noreply.github.com>
2020-09-10 01:01:20 +0200

    Add files via upload

---
+++ client/build/precache-manifest.8a6480d4d04526e81b880d5af693a4e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37018cb32556179708d05fb1bdce977d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32c55f8bd7e29b0d16e3",
+    "url": "/static/css/main.8fa68445.chunk.css"
+  },
+  {
+    "revision": "0ff83ede418c779bcd47",
+    "url": "/static/js/2.e1445340.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.e1445340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c55f8bd7e29b0d16e3",
+    "url": "/static/js/main.e8039af3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
