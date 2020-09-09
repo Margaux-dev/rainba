465e793a332d687ebd1336f766589e8802b6da05
Margaux Darriberouge <65903630+Margaux-dev@users.noreply.github.com>
2020-09-10 01:14:25 +0200

    Update Chat.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -14,7 +14,7 @@ const Chat = ({location}) => {
     const [users, setUsers] = useState("");
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = "https://rainba.herokuapp.com/"
+    const ENDPOINT = "https://rainba.herokuapp.com/";
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
